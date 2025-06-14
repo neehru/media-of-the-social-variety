001ebc1b07456ed1584ef5e690f58046fdc271e2
Neehru Tumber <neehru25.tumber@gmail.com>
2025-06-14 15:24:52 +0100

    added name, birthday and visibility to user

--- app/src/main/java/neehru/app/service/CustomAuthenticationSuccessHandler.java
+++ app/src/main/java/neehru/app/service/CustomAuthenticationSuccessHandler.java
@@ -1,4 +1,27 @@
 package neehru.app.service;
 
-public class CustomAuthenticationSuccessHandler {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication)
+            throws IOException {
+
+        String redirect = "/dashboard";
+
+        // add redirect depending on role heirarchy if i add
+
+
+    }
 }
